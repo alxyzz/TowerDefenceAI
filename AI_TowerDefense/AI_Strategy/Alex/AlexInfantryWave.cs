70c9a1d088956e31cc2ee1ab5b8fc74a961b143c
alxyzz <93402865+alxyzz@users.noreply.github.com>
2024-11-26 14:47:50 +0100

    doing it differently actually

---
+++ AI_TowerDefense/AI_Strategy/Alex/AlexInfantryWave.cs
@@ -0,0 +1,51 @@
+﻿using GameFramework;
+using System.Collections.Generic;
+
+namespace AI_Strategy
+{
+    /*
+     * This class derives from Soldier and provides a new move method. Your assignment should
+     * do the same - but with your own movement strategy.
+     */
+    public class AlexInfantryWave : Soldier
+    {
+
+        //say we have a weak spot with a coverage of 3 towers for 5 tiles.
+        //15 shots of 2hp each. a soldier has 6 HP. so we need 5 soldiers to pass thru. a column of 6 units can pass thru that and still reach the enemy base.
+        //but obviously they'll have a ton of towers...
+        // i need to find the optimal height/width for a platoon of soldiers, because surely there's a better way than just filling the row horizontally like some weird napoleon warfare firing line lol
+        //movement wise, it's probably better to just let them move as normal. since the strategy lies in their placement
+
+        //enum mindstate
+        //{
+        //    hunter, //hunts the closest tower as a group and runs if it's low on health (to avoid enemy getting gold)
+        //    worm, //agreggate as a column and move through the weakest spot
+        //    stash, //inactive - deny the enemy their hard-earned gold. 
+        //    reset//process situation and see what to do
+        //}
+
+
+        //mindstate mind = mindstate.reset; //default to reset
+
+        public override void Move()
+        {
+            if (speed > 0 && posY < PlayerLane.HEIGHT)
+            {
+                int x = posX;
+                int y = posY;
+                for (int i = speed; i > 0; i--)
+                {
+                    if (MoveTo(x, y + i)) return;
+                    if (MoveTo(x + i, y + i)) return;
+                    if (MoveTo(x - i, y + i)) return;
+                    if (MoveTo(x + i, y)) return;
+                    if (MoveTo(x - i, y)) return;
+                    if (MoveTo(x, y - i)) return;
+                    if (MoveTo(x - i, y - i)) return;
+                    if (MoveTo(x + i, y - i)) return;
+                }
+            }
+
+        }
+    }
+}
--- AI_TowerDefense/AI_Strategy/Alex/AlexSoldierPlacementStrategy.cs
+++ AI_TowerDefense/AI_Strategy/Alex/AlexSoldierPlacementStrategy.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using GameFramework;
+﻿using GameFramework;
+using System;
 using System.Collections.Generic;
 
 namespace AI_Strategy
@@ -7,38 +7,81 @@ namespace AI_Strategy
     /*
      * very simple example strategy based on random placement of units.
      */
-    public class AlexStrategyLoggerDemo : AbstractStrategy
+    public class AlexSoldierPlacementStrategy : AbstractStrategy
     {
+        
+
+
+        private static AlexSoldierPlacementStrategy _instance;
+        private static readonly object _lock = new object();
+        public static AlexSoldierPlacementStrategy GetInstance(Player player)
+        {
+            if (_instance == null)
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new AlexSoldierPlacementStrategy(player);
+                    }
+                }
+            }
+            return _instance;
+        }
         private int messageCounter = 1;
+
+        private static List<Soldier> worms = new(); //list of squads. a solo tower can fire on a solo soldier 3 times to kill it- but a soldier needs 5 turns to pass through a tower's range. 
+        //this means that a tower needs to be matched with 2 intact soldiers to pass thru. then we should also find the weakest possible spot
+
+        List<Cell> columnPositions = new();//to fill out with troop ranks. if all full, 
+
         private static Random random = new Random();
 
-        public AlexStrategyLoggerDemo(Player player) : base(player)
+        public AlexSoldierPlacementStrategy(Player player) : base(player)
         {
+
         }
 
         /*
          * example strategy for deploying Towers based on random placement and budget.
          * Your one should be better!
          */
+
+        public bool  AddSoldierToWorm(AlexInfantryWave me)
+        {
+            if (worms.Count < 8)
+            {
+                worms.Add(me);
+                return true;
+            }
+            else return false;
+            
+        }
+
+
+        //places towers  as evenly distributed as possible, and should always have about 2 spaces below them to maximize firing area
+        //also, no towers should be placed on the outermost 2 tiles unless there's no space anywhere else
         public override void DeployTowers()
         {
             if (player.Gold > 8)
             {
-                bool positioned = false;
+                //Boolean positioned = false;
                 int count = 0;
-                while (!positioned && count < 20)
+                while (/*!positioned && */count < 6)
                 {
                     count++;
-                    int x = random.Next(PlayerLane.WIDTH);
-                    int y = random.Next(PlayerLane.HEIGHT - PlayerLane.HEIGHT_OF_SAFETY_ZONE) + PlayerLane.HEIGHT_OF_SAFETY_ZONE;
+                    int x = random.Next(PlayerLane.WIDTH +2, PlayerLane.WIDTH-2);
+                    int y = random.Next(PlayerLane.HEIGHT - 1) + 1; // has to leave soldier deploy lane empty
                     if (player.HomeLane.GetCellAt(x, y).Unit == null)
                     {
-                        positioned = true;
+                        //positioned = true;
                         player.TryBuyTower<Tower>(x, y);
                     }
                 }
             }
+
         }
+
 
         /*
          * example strategy for deploying Soldiers based on random placement and budget.
@@ -46,8 +89,12 @@ namespace AI_Strategy
          */
         public override void DeploySoldiers()
         {
+            int round = 0;
+            //if gold is over 30 - spawn 6 soldiers as a squad
 
-          //DebugLoger.Log(Tower.GetNextTowerCosts(defendLane));
+
+
+            //DebugLoger.Log(Tower.GetNextTowerCosts(defendLane));
             DebugLogger.Log("#" + messageCounter + " Deployed Soldier!");
             messageCounter++;
 
@@ -60,26 +107,38 @@ namespace AI_Strategy
                 System.Threading.Thread.Sleep(50);
             }
 
-            int round = 0;
+
             while (player.Gold > 5 && round < 5)
             {
+
                 round++;
                 bool positioned = false;
                 int count = 0;
                 while (!positioned && count < 10)
                 {
                     count++;
+
                     int x = random.Next(PlayerLane.WIDTH);
                     int y = 0;
                     if (player.EnemyLane.GetCellAt(x, y).Unit == null)
                     {
                         positioned = true;
-                        player.TryBuySoldier<MySoldier>(x, out var soldier);
+
+                        if (player.Gold < 30)
+                        {
+                            for (int i = 0; i < 5; i++)
+                            {
+                                player.TryBuySoldier<AlexInfantryWave>(x);
+                            }
+
+                        }
+                        else
+                        { player.TryBuySoldier<AlexInfantryWave>(x); }
+
                     }
                 }
             }
         }
-
         /*
          * called by the game play environment. The order in which the array is returned here is
          * the order in which soldiers will plan and perform their movement.
@@ -102,5 +161,6 @@ namespace AI_Strategy
         {
             return unsortedList;
         }
+
     }
 }
